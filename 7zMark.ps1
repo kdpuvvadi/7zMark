866bf56c0acbd16ac0cb71aa2ef194e0252bc5b9
KD Puvvadi <kdpuvvadi@gmail.com>
2021-10-21 16:24:32 +0530

    benchmark

--- 7zMark.ps1
+++ 7zMark.ps1
@@ -1,25 +1,25 @@
-param($type="7z",$setupclean)
+param(
+        $type="7z",
+        $setupclean="n"
+    )
 
 if( "7z","zip" -NotContains $type ) { Throw "$type is not valid method" }
-if( "y","Y","n","N" -notcontains $setupclean ) { Throw "$type is not valid input" }
+if( "y","Y","n","N" -notcontains $setupclean ) { Throw "$setupclean is not valid input" }
 
 $Folder = "temp"
 $method = "-t$($type)"
 Write-Output "Starting"
 
 function startBenchMark {
-    param (
-        $timeDuration
-    )
     $7ztime = Measure-Command -Expression { 7z a -mmt -mx9 $method test.$($type) .\$($Folder) | Out-Default } | select-object -Expand TotalMinutes 
-    return $7ztime
+    Write-Output "Time for completion $($7ztime.ToString("###.####")) Minutes"
 }
 
 $getSize = (Get-ChildItem .\temp\ | Measure-Object -Property Length -Sum ).Sum / 1048576
 $sizeofDir = $getSize.ToString("###.##")
 
 $startBench = Read-Host -Prompt "Size of dump is $($sizeofDir)Mib, Start Benchmark ?[y/n]"
-if ($startBench -match "[yY]") {startBenchMark($time); Write-Output "Time for completion $time "}
+if ($startBench -match "[yY]") {startBenchMark}
 
 if ( $setupclean -match "[yY]" ) { 
     Write-Output "Cleaning up"
