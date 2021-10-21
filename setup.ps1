2c174a9161cfb77ae786841e631ff9bfe4223076
KD Puvvadi <kdpuvvadi@gmail.com>
2021-10-21 15:38:32 +0530

    all to func

--- setup.ps1
+++ setup.ps1
@@ -1,18 +1,46 @@
 param([int32] $fileCount=1000, $lines=100)
 $Folder = "temp"
-New-Item -ItemType Directory $Folder -Force| out-null
-$filename="file.txt"
-$quickbrown
-for ($x=1; $x -le $lines; $x++) { 
-    Add-Content -Value  '$quickbrown `n' -Path $Folder\$($filename)
-    Write-Progress -Activity "File Creation" -Status "addeding content" -PercentComplete (($x/$lines)*100)
 
+function createFolder {
+    New-Item -ItemType Directory $Folder -Force| out-null
 }
 
-for ($k=1; $k -le $fileCount; $k++) {
-    
-    Copy-Item $Folder\$($filename) -Destination $Folder\file$($k).txt
-    Write-Progress -Activity "File copy" -Status "copying:" -PercentComplete (($k/$fileCount)*100)
+function createTextFile {
+    $filename="file.txt"
+    $quickbrown = "The quick brown fox jumps over the lazy dog"
+    for ($x=1; $x -le $lines; $x++) { 
+        Add-Content -Value  "$($quickbrown) `n" -Path $Folder\$($filename)
+        Write-Progress -Activity "File Creation" -Status "addeding content" -PercentComplete (($x/$lines)*100)
+    }
+}
+function createdump {
+    for ($k=1; $k -le $fileCount; $k++) {
+        Copy-Item $Folder\$($filename) -Destination $Folder\file$($k).txt
+        Write-Progress -Activity "File copy" -Status "copying:" -PercentComplete (($k/$fileCount)*100)
+    }
+    if($?) { Write-Output "Setup Completed"} else { Write-Error -Message "Error" -ErrorId 1}
 }
 
-if($?) { Write-Output "Setup Completed"} else { Write-Error -Message "Error" -ErrorId 1}
+if (Test-Path -Path $Folder) {
+
+    $folderexists = Read-Host -Prompt "Test dumt already exists, Continue to create dump files?[y/n]"
+    if ( $folderexists -match "[yY]" ) { 
+
+        remove-item $Folder  -Force -Recurse -Confirm:$false
+        Start-Sleep -Milliseconds 1500
+        createFolder
+        Start-Sleep -Milliseconds 1500
+        createTextFile
+        Start-Sleep -Milliseconds 1500
+        createdump
+    }
+    elseif ($folderexists -notmatch	 "[yY]") { exit 0 }
+}
+else { 
+    Start-Sleep -Milliseconds 1500
+    createFolder
+    Start-Sleep -Milliseconds 1500
+    createTextFile
+    Start-Sleep -Milliseconds 1500
+    createdump
+ }
