f3bc5f4aa2776a322c122df107cbd2bc40975cad
KD Puvvadi <kdpuvvadi@gmail.com>
2021-10-21 15:58:10 +0530

    fixing folder creation

--- setup.ps1
+++ setup.ps1
@@ -2,20 +2,19 @@ param([int32] $fileCount=1000, $lines=100)
 $Folder = "temp"
 
 function createFolder {
-    New-Item -ItemType Directory $Folder -Force| out-null
+    New-Item -ItemType Directory $Folder -Force | out-null
 }
 
 function createTextFile {
-    $filename="file.txt"
-    $quickbrown = "The quick brown fox jumps over the lazy dog"
+    New-Item -Path ./$Folder -Name "file.txt" -ItemType "file" -Value "The quick brown fox jumps over the lazy dog.`n" | out-null
     for ($x=1; $x -le $lines; $x++) { 
-        Add-Content -Value  "$($quickbrown) `n" -Path $Folder\$($filename)
+        Add-Content -Value 'The quick brown fox jumps over the lazy dog' -Path $Folder\file.txt
         Write-Progress -Activity "File Creation" -Status "addeding content" -PercentComplete (($x/$lines)*100)
     }
 }
 function createdump {
     for ($k=1; $k -le $fileCount; $k++) {
-        Copy-Item $Folder\$($filename) -Destination $Folder\file$($k).txt
+        Copy-Item $Folder\file.txt -Destination $Folder\file$($k).txt
         Write-Progress -Activity "File copy" -Status "copying:" -PercentComplete (($k/$fileCount)*100)
     }
     if($?) { Write-Output "Setup Completed"} else { Write-Error -Message "Error" -ErrorId 1}
