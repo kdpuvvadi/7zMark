a27cfd0a2fd326753313bbe9dd2897da3f4051fa
KD Puvvadi <kdpuvvadi@gmail.com>
2021-10-21 10:54:42 +0530

    input validation & cleanup in single

--- 7zMark.ps1
+++ 7zMark.ps1
@@ -1,5 +1,8 @@
 param($type="7z")
 
+if( "7z","zip" -NotContains $type ) { Throw "$type is not valid method" }
+
+$Folder = "temp"
 $method = "-t$($type)"
 Write-Output "Starting"
 $7ztime = Measure-Command -Expression { 7z a -mmt -mx9 $method test.$($type) .\$($Folder)\ | Out-Default } 
@@ -9,11 +12,12 @@ $duration =$7ztime | select-object -Expand TotalMinutes
 if($?) {Write-Output "Completed"}
 
 Write-Output "Operation took $duration minutes"
-Write-Output "Deleting Out file"
-remove-item test.$($type) -Force -Confirm:$false
+
 
 $cleanup = Read-Host -Prompt "Delete setup files?[y/n]"
 if ( $cleanup -match "[yY]" ) { 
+    Write-Output "Cleaning up"
+    remove-item test.$($type) -Force -Confirm:$false
     remove-item ./temp/  -Force -Recurse -Confirm:$false
 }
 else {
