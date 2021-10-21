aae508006779297c8dec4ae4455943404823f60a
KD Puvvadi <kdpuvvadi@gmail.com>
2021-10-21 16:01:35 +0530

    gb to mb

--- 7zMark.ps1
+++ 7zMark.ps1
@@ -1,4 +1,4 @@
-param($type="7z")
+param($type="7z",$setupclean)
 
 if( "7z","zip" -NotContains $type ) { Throw "$type is not valid method" }
 
@@ -10,14 +10,14 @@ function startBenchMark {
     param (
         $timeDuration
     )
-    $7ztime = Measure-Command -Expression { 7z a -mmt -mx9 $method test.$($type) .\$($Folder)\ | Out-Default } | select-object -Expand TotalMinutes 
+    $7ztime = Measure-Command -Expression { 7z a -mmt -mx9 $method test.$($type) .\$($Folder) | Out-Default } | select-object -Expand TotalMinutes 
     return $7ztime
 }
 
-$getSize = (Get-ChildItem .\temp\ | Measure-Object -Property Length -Sum ).Sum / 1073741824
+$getSize = (Get-ChildItem .\temp\ | Measure-Object -Property Length -Sum ).Sum / 1048576
 $sizeofDir = $getSize.ToString("###.##")
 
-$startBench = Read-Host -Prompt "Size of dump is $($sizeofDir)Gb, Start Benchmark ?[y/n]"
+$startBench = Read-Host -Prompt "Size of dump is $($sizeofDir)Mib, Start Benchmark ?[y/n]"
 if ($startBench -match "[yY]") {startBenchMark($time); Write-Output "Time for completion $time "}
 
 $cleanup = Read-Host -Prompt "Delete setup files?[y/n]"
