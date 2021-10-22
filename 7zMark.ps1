17d70394a7b1f022ba511cc9e18790407a94b03c
KD <kdpuvvadi@gmail.com>
2021-10-22 17:30:21 +0530

    setup from mark, error if benchmark fails

--- 7zMark.ps1
+++ 7zMark.ps1
@@ -15,11 +15,29 @@ function startBenchMark {
     Write-Output "Time for completion $($7ztime.ToString("###.####")) Minutes"
 }
 
+function setup {
+    $dumpStart = Read-Host -Prompt "Test Dump is not present. Press Y/y to create?[y/n]"
+    if ($dumpStart -match "[yY]") {
+        $NoLines = Read-Host -Prompt "Select Number of lines:"
+        $NoFiles = Read-Host -Prompt "Enter number of files:"
+        Invoke-Expression "./setup.ps1 -fileCount $NoFiles -lines $NoLines"
+        if(!$?){ Throw Write-Output "Something went wrong" }
+    }
+}
+
+$dumpExist = Test-Path $Folder
+if(!$dumpExist) {
+    setup
+}
+
 $getSize = (Get-ChildItem .\temp\ | Measure-Object -Property Length -Sum ).Sum / 1048576
 $sizeofDir = $getSize.ToString("###.##")
 
-$startBench = Read-Host -Prompt "Size of dump is $($sizeofDir)Mib, Start Benchmark ?[y/n]"
-if ($startBench -match "[yY]") {startBenchMark}
+$startBench = Read-Host -Prompt "Size of dump is $($sizeofDir)Mb, Start Benchmark ?[y/n]"
+if ($startBench -match "[yY]") {
+    startBenchMark
+    if(!$?){ Throw Write-Output "Something went wrong" }
+    }
 
 if ( $setupclean -match "[yY]" ) { 
     Write-Output "Cleaning up"
